1b789475712150e5f23fccd66bcd792df8edb9bf
Suraj biswas <surajbiswas182003@gmail.com>
2024-11-06 09:20:25 +0530

    added

--- Main.cpp
+++ Main.cpp
@@ -1,75 +1,25 @@
 #include <iostream>
 #include "Tree/Tree.h"
-
 using namespace std;
 
+
 int main() {
-  Node* node1 = new Node(1, 2, 3);
-  Node* node2 = new Node(5, 6, 7);
-  Node* root = new Node(4, node1, node2);
+	Node* node1 = new Node(1, 2, 3);
+	Node* node2 = new Node(5, 6, 7);
+	// Node* node3 = new Node(5);
+	Node* root = new Node(node1, 4, node2);
+	BinaryTree tree(root);
+	tree.print();
+	// int item;
+	// cin >> item;
+	Node* node = tree.subTreeFirst(root);
+	cout << node << endl;
+	// cin >> item;
+	node = tree.subTreeLast(root);
+	cout << node << endl;
 
-  BinaryTree tree(root);
-  cout << tree << endl;
-  /*
-   * TODO review all the functions
-   */
+	tree.subTreeFirstTest(root);
+	tree.subTreeLastTest(root);
 
-  int item;
-  // cout << "Enter a item: ";
-  // cin >> item;
-  // Node* key1 = tree.findNode(item);
-  // if (key1 == nullptr) { cout << key1 << " is not present" << endl; }
-  // else {
-  //   cout << "SubTreeFirst of node " << item << " is " << tree.subTreeFirst(key1)->item << endl;
-  //   cout << "SubTreeLast of node " << item << " is " << tree.subTreeLast(key1)->item << endl;
-  // }
-  //
-  // cout << "Enter a item: ";
-  // cin >> item;
-  // Node* key2 = tree.findNode(item);
-  // if (key2 == nullptr) {
-  //   cout << key2 << " is not present" << endl;
-  // }
-  // else {
-  //   Node* successor = tree.successor(key2);  // Store successor in `succ`
-  //   Node* predecessor = tree.predecessor(key2); // Store predecessor in `pred`
-  //
-  //   cout << "Successor of node " << item << " is ";
-  //   if (successor == nullptr) {
-  //     cout << "NULL" << endl;
-  //   } else {
-  //     cout << successor -> item << endl;
-  //   }
-  //   cout << "Predecessor of node " << item << " is ";
-  //   if (predecessor == nullptr) {
-  //     cout << "NULL" << endl;
-  //   }
-  //   else {
-  //     cout << predecessor->item << endl;
-  //   }
-  // }
-  //
-  // cout << "Enter a item to insert after root: ";
-  // cin >> item;
-  // Node* newNode = new Node(item);
-  // tree.insertAfter(root, newNode);
-  // cout << "Inserted " << item << " after " << root->item << endl;
-  // cout << tree << endl;
-  //
-  // cout << "Enter a item to insert before root: ";
-  // cin >> item;
-  // newNode = new Node(item);
-  // tree.insertBefore(root, newNode);
-  // cout << "Inserted " << item << " before " << root->item << endl;
-  // cout << tree << endl;
-
-  Node* n;
-  cout << "Enter a item to delete: ";
-  cin >> item;
-  n = tree.findNode(item);
-  tree.deleteNode(n);
-  cout << tree << endl;
-
-
-  return 0;
+	return 0;
 }
--- Tree/Tree.h
+++ Tree/Tree.h
@@ -3,261 +3,161 @@
 using namespace std;
 
 class Node{
-  public:
-    int item;
-    Node* parent;
-    Node* left;
-    Node* right;
+	public:
+		int item;
+		Node* parent;
+		Node* left;
+		Node* right;
+		Node(int item) {
+			this->item = item;
+			this->parent = nullptr;
+			this->left = nullptr;
+			this->right = nullptr;
+		}
 
-    Node(int item) {
-      this->item = item;
-      this->parent = nullptr;
-      this->left = nullptr;
-      this->right = nullptr;
-    }
+		Node(Node* left, int item, Node* right) {
+			this->item = item;
+			this->left = left;
+			this->right = right;
+			left->parent = this;
+			right->parent = this;
+		}
+		Node(int left, int item, int right) {
+			Node* leftNode = new Node(left);
+			Node* rightNode = new Node(right);
+			this->item = item;
+			this->left = leftNode;
+			this->right = rightNode;
+			leftNode->parent = this;
+			rightNode->parent = this;
+		}
 
-    Node(int item, Node* left, Node* right) {
-      this->item = item;
-      this->left = left;
-      this->right = right;
-      left->parent = this;
-      right->parent = this;
-    }
+		bool hasLeftChild() {
+			return left != nullptr;
+		}
+		bool hasRightChild() {
+			return right != nullptr;
+		}
 
-    Node(int leftItem, int item, int rightItem) {
-      Node* left = new Node(leftItem);
-      Node* right = new Node(rightItem);
-      this->item = item;
-      this->left = left;
-      this->right = right;
-      left->parent = this;
-      right->parent = this;
-    }
+		bool isLeaf() {
+			return right == nullptr && left == nullptr;
+		}
 
-    bool hasLeftChild() {
-      return left != nullptr;
-    }
+		bool isRoot() {
+			return parent == nullptr;
+		}
 
-    bool hasRightChild() {
-      return right != nullptr;
-    }
-
-    bool isLeaf() {
-      return left == nullptr && right == nullptr;
-    }
-
-    bool isRoot() {
-      return parent == nullptr;
-    }
-
-    bool isLeftChild() {
-      if (isRoot()) {
-        return false;
-      }
-      else {
-        return parent->left->item == item;
-      }
-    }
-
-    bool isRightChild() {
-      if (isRoot()) {
-        return false;
-      }
-      else {
-        return parent->right->item == item;
-      }
-    }
-
-
+		friend ostream& operator<<(ostream& out, Node* node);
 };
 
-
-class BinaryTree{
-  private:
-    Node* root;
-
-  public:
-    BinaryTree(Node* root) {
-      this->root = root;
-    }
-
-    Node* subTreeFirst(Node* node) {
-      if (node == nullptr) {
-        return nullptr;
-      }
-      if (node->hasLeftChild()) {
-        while (!(node->isLeaf())) {
-          node = node->left;
-        }
-      }
-      return node;
-    }
-
-    Node* subTreeLast(Node* node) {
-      if (node == nullptr) {
-        return nullptr;
-      }
-      if (node->hasRightChild()) {
-        while (!(node->isLeaf())) {
-          node = node->right;
-        }
-      }
-      return node;
-    }
-
-
-    Node* successor(Node* node) {
-      if (node == nullptr) {
-        return nullptr;
-      }
-      if (node->hasRightChild()) { 
-        return subTreeFirst(node->right);
-      } else {
-        while(node != node->parent->left) {
-          node = node->parent;
-          if (node->parent == nullptr) {
-            return nullptr;
-          }
-        }
-        return node->parent;
-      }
-    }
-
-    Node* predecessor(Node* node) {
-      if (node == nullptr) {
-        return nullptr;
-      }
-      if (node->hasLeftChild()) {
-        return subTreeLast(node->left);
-      } else {
-        while (node != node->parent->right) {
-          node = node -> parent;
-          if (node -> parent == nullptr) {
-            return nullptr;
-          }
-        }
-        return node->parent;
-      }
-    }
-
-    void insertAfter(Node* node, Node* newNode) {
-      if (!node->hasRightChild()) {
-        node->right = newNode;
-        newNode->parent = node;
-      } else {
-        Node* nextNode = subTreeFirst(node->right);
-        nextNode->left = newNode;
-        newNode->parent = nextNode;
-      }
-    }
-
-    void insertBefore(Node* node, Node* newNode) {
-      if (!node->hasLeftChild()) {
-        node->left = newNode;
-        newNode->parent = node;
-      } else {
-        Node* prevNode = subTreeLast(node->left);
-        prevNode->right = newNode;
-        newNode->parent = prevNode;
-      }
-    }
-    int deleteNode(Node* node) {
-      if (node == nullptr) {
-        return -1;
-      }
-      int item = node->item;
-
-      if (node->isLeaf()) {
-        if (node->isRoot()) { 
-          delete node;
-          root = nullptr;
-        }
-        else {
-          Node* tempParent = node->parent;
-          bool isLeft = node->isLeftChild();
-          delete node;
-          if (isLeft) {
-            tempParent->left = nullptr;
-          }
-          else {
-            tempParent->right = nullptr;
-          }
-        }
-        return item;
-      }
-      else {
-        if (node->hasLeftChild()) {
-          Node* prev = predecessor(node);
-          // cout << "predecessor "  << "of " << node->item << " -> " << prev->item << endl;
-          swap(node->item, prev->item);
-          deleteNode(prev);
-        }
-        else {
-          Node* next = successor(node);
-          swap(node->item, next->item);
-          deleteNode(next);
-        }
-        return item;
-      } 
-    }
-
-    Node* findNode(int item) {
-      return findNode(root, item);
-    }
-
-    bool hasPredecessor(Node* node) {
-      return predecessor(node) != nullptr;
-    }
-
-    bool hasSuccessor(Node* node) {
-      return successor(node) != nullptr;
-    }
-
-    friend ostream& operator<<(ostream& out,const BinaryTree& tree);
-
-    ~BinaryTree() {
-      deleteTree(root);
-    }
-
-  private:
-    void print(Node* node, ostream& out) const{
-      if (node == nullptr) {
-        return;
-      } else {
-        print(node->left, out);
-        out << node->item << " ";
-        print(node->right, out);
-      }
-    }
-
-    void deleteTree(Node* node) {
-      if (node == nullptr) {
-        return;
-      } else {
-        deleteTree(node->left);
-        deleteTree(node->right);
-        delete node;
-      }
-    }
-
-    Node* findNode(Node* root, int item) {
-      if (root == nullptr || root->item == item) {
-        return root;
-      }
-      else {
-        Node* left = findNode(root->left, item);
-        Node* right = findNode(root->right, item);
-        if (left != nullptr) {
-          return left;
-        }
-        else {
-          return right;
-        }
-      }
-    }
-};
-
-inline ostream& operator<<(ostream& out, const BinaryTree& tree) {
-  tree.print(tree.root, out);
-  return out;
+inline ostream& operator<<(ostream& out, Node* node) {
+	if (node != nullptr)
+		out << node->item << " ";
+	else 
+		out << "null ";
+	return out;
 }
+
+class BinaryTree {
+		
+	public:
+		Node* root;
+		BinaryTree(Node* root) {
+			this->root = root;
+		}
+
+		Node* subTreeFirst(Node* node) {
+			if (node == nullptr || !(node->hasLeftChild()) || node->isLeaf()) { return node; }
+			else {
+				return subTreeFirst(node->left);
+			}
+		}
+
+		void subTreeFirstTest(Node* node) {
+			if (node == nullptr) {
+				return;
+			} else {
+				Node* res = subTreeFirst(node);
+				cout << "SubTreeFirst of " << node  << " is " << res << endl;
+				subTreeFirstTest(node->left);
+				subTreeFirstTest(node->right);
+			}
+		}
+
+		Node* subTreeLast(Node* node)  {
+			if (node == nullptr || !(node->hasRightChild()) || node->isLeaf()) {
+				return node;
+			}
+			else {
+				return subTreeLast(node->right);
+			}
+		}
+		void subTreeLastTest(Node* node) {
+			if (node == nullptr) {
+				return;
+			} else {
+				Node* res = subTreeLast(node);
+				cout << "SubTreeLast of " << node  << " is " << res << endl;
+				subTreeLastTest(node->left);
+				subTreeLastTest(node->right);
+			}
+		}
+
+		Node* succcessor(Node* node) {
+			if (node == nullptr){
+				return nullptr;
+			}
+			else if (node->hasRightChild()){
+				return subTreeFirst(node->right);
+			}
+			else if (node->isRoot()){
+				return nullptr;
+			}
+			else {
+				while (node->parent->left != node) {
+					node = node->parent;
+				}
+				return node;
+			}
+		}
+
+		// Node* find(int item) {
+		// 	if (root == nullptr) {
+		//
+		// 	}
+		// }
+
+
+		void print() {
+			print(root);
+			cout << endl;
+		}
+
+		~BinaryTree() {
+			deleteTree(root);
+		}
+
+	private:
+		void print(Node* root) {
+			if (root == nullptr) {
+				return;
+			}
+			else {
+				print(root->left);
+				cout << root << " ";
+				print(root->right);
+			}
+		}
+
+		void deleteTree(Node* root) {
+			if (root == nullptr) {
+				return;
+			}
+			else {
+				deleteTree(root->left);
+				deleteTree(root->right);
+				delete root;
+			}
+		}
+};
+
